8f310255ac4791dcbe98bf28f12998d5eb7551b8
Lucas Opoka <lucasopoka@icloud.com>
2023-05-20 21:00:58 +0300

    py_runner_up
    Added solution to the Runner-up challenge.

---
+++ Basic Data Types/py_runner_up.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = sorted(set(map(int, input().split())))
+
+    print(arr[-2])
