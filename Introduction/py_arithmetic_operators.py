486b80ae53d208ee3b8115f0d82872a9679bbdcc
Lucas Opoka <lucasopoka@icloud.com>
2023-05-20 20:24:02 +0300

    py_arithmetic_operators
    Added solution to the Arithmetic Operators challenge.

---
+++ Introduction/py_arithmetic_operators.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum_ab = a+b
+    difference_ab = a - b
+    product_ab = a * b
+    
+    print(str(sum_ab))
+    print(str(difference_ab))
+    print(str(product_ab))
