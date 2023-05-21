562871e433562caa60fca19a781a2d0dc7046d9a
Lucas Opoka <lucasopoka@icloud.com>
2023-05-21 13:28:30 +0300

    py_mod_divmod
    Added solution to the Mod Divmod challenge.

---
+++ 5. Math/py_mod_divmod.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/python-mod-divmod
+
+from __future__ import division
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a % b)
+print(f'({a // b}, {a % b})')
