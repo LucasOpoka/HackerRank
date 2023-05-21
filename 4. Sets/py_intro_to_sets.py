3e622aa2d4c742e11f0de786882315564ace8ef1
Lucas Opoka <lucasopoka@icloud.com>
2023-05-21 10:50:26 +0300

    py_intro_to_sets
    Added solution to the Introduction to Sets challenge.

---
+++ 4. Sets/py_intro_to_sets.py
@@ -0,0 +1,11 @@
+#https://www.hackerrank.com/challenges/py-introduction-to-sets
+
+def average(array):
+    array = set(array)
+    return sum(array) / len(array)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
