7205bce68f36dd92a64eef57c87b335a4a2a7723
Lucas Opoka <lucasopoka@icloud.com>
2023-05-24 11:45:40 +0300

    py_decorators_2_name_directory.py
    Added solution to the Decorators 2 - Name Directory challenge.

---
+++ 1. Python Course/15. Closures and Decorators/py_decorators_2_name_directory.py
@@ -0,0 +1,17 @@
+#https://www.hackerrank.com/challenges/decorators-2-name-directory
+
+import operator
+
+def person_lister(f):
+    def inner(people):
+        people.sort(key=lambda x:int(x[2]), reverse=False)
+        return [f(p) for p in people]
+    return inner
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
